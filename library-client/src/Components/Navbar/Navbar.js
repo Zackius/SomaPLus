d60669b0ec57ab3a23a0730e9479b6ed5c097874
Zacharia <ndunguzachary24@gmail.com>
2022-11-22 04:57:51 +0300

    Navbar Mobile responsiveness

--- library-client/src/Components/Navbar/Navbar.js
+++ library-client/src/Components/Navbar/Navbar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { logo } from "../../assests/";
 import { Link } from "react-router-dom";
+import{FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
-  return (
+
+    const [nav, setNav] = useState(false);
+
+    const handleClick = () => setNav(!nav);
+    return (
+    //   Web Navbar
     <div className="fixed w-full h-[80px] flex  justify-evenly  items-center  px-4  bg-[#6be7d9]">
       <div>
         <img
@@ -44,7 +50,53 @@ const Navbar = () => {
             Contact
           </Link>
         </li>
-      </ul>
+            </ul>
+            {/* Closing and Opening in mobile navbar */}
+            <div onClick={handleClick} className=" md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+            {/* Mobile Navbar */}
+            <div
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen  bg-[#6be7d9] flex flex-col  justify-center items-center"
+        }
+      >
+        <ul>
+        <li className="py-6 text-4xl">
+          <Link to="home" smooth={true} duration={300}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link to="about" smooth={true} duration={300}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link to="skills" smooth={true} duration={300}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link to="work" smooth={true} duration={300}>
+            Work
+          </Link>
+          </li>
+          <li className="py-6 text-4xl">
+          <Link to="/" smooth={true} duration={300}>
+            Blog
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link to="contact" smooth={true} duration={300}>
+            Contact
+          </Link>
+          </li>
+        </ul>
+      </div>
+
     </div>
   );
 };
